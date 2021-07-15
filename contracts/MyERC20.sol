185dd3dc3a32ac670bc1b8efcef5a173122a71d8
ethan.1q <ethan.1q@kakaocorp.com>
2021-07-15 18:14:09 +0900

    Rename contract name

--- contracts/MyERC20.sol
+++ contracts/MyERC20.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
+contract TestToken is ERC20 {
     constructor() ERC20("TestToken", "TEST") {
         _mint(msg.sender, 1e24);
     }
