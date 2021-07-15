8df69f42cb87fc2129b4086ff312b507eacf6631
ethan.1q <ethan.1q@kakaocorp.com>
2021-07-15 15:33:10 +0900

    주석 추가

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ from util import load_depolyed_contract
 w3 = Web3(HTTPProvider('https://api.baobab.klaytn.net:8651'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
+# 컨트랙트 배포자 주소
 contract_owner_address = w3.toChecksumAddress('0x5c1e67e5efaab009636dc332cfaea990b0cb156f')
+# 컨트랙트 배포자 private key
 contract_owner_priavte_key = '0x19a0e6c99aaef89900278e29af962e35bab66394312e55d891a97d51e781cda0'
+# 토큰 전송 테스트용 계정 주소
 test_address = w3.toChecksumAddress('0x9e06bf1eca9e3194141f7bede658a8d344614db9')
 
 # 배포
