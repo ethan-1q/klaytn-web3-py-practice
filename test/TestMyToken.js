9fbfbba0dba6d9167e2c28d00f3f18ba059ae44a
ethan.1q <ethan.1q@kakaocorp.com>
2021-07-21 16:31:12 +0900

    Update TestMyToken.js

--- test/TestMyToken.js
+++ test/TestMyToken.js
@@ -1,25 +1,24 @@
-const { expect } = require('chai');
+// Reference1: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/test/token/ERC20/ERC20.test.js
+// Reference2: https://github.com/OpenZeppelin/openzeppelin-test-helpers
 
-// Import utilities from Test Helpers
+// Import utilities
+const { expect } = require('chai');
 const { BN, expectEvent, expectRevert, constants } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
 
 // Load compiled artifacts
 const MyToken = artifacts.require('MyToken');
 
 // Start test block
-contract('MyToken', function ([ creator, other ]) {
+contract('MyToken', function (accounts) {
+  const [ initialHolder, recipient, anotherAccount ] = accounts;
 
   const NAME = 'MyToken';
   const SYMBOL = 'TEST';
   const TOTAL_SUPPLY = new BN('10000000000000000000000');
 
   beforeEach(async function () {
-    this.token = await MyToken.new(NAME, SYMBOL, TOTAL_SUPPLY, { from: creator });
-  });
-
-  it('retrieve returns a value previously stored', async function () {
-    // Use large integer comparisons
-    expect(await this.token.totalSupply()).to.be.bignumber.equal(TOTAL_SUPPLY);
+    this.token = await MyToken.new(NAME, SYMBOL, TOTAL_SUPPLY, {from: initialHolder});
   });
 
   it('has a name', async function () {
@@ -30,7 +29,50 @@ contract('MyToken', function ([ creator, other ]) {
     expect(await this.token.symbol()).to.be.equal(SYMBOL);
   });
 
-  it('assigns the initial total supply to the creator', async function () {
-    expect(await this.token.balanceOf(creator)).to.be.bignumber.equal(TOTAL_SUPPLY);
+  it('has 18 decimals', async function () {
+    expect(await this.token.decimals()).to.be.bignumber.equal('18');
+  });
+
+  it('has 10000 tokens', async function () {
+    expect(await this.token.totalSupply()).to.be.bignumber.equal(TOTAL_SUPPLY);
+  });
+
+  it('assigns the initial total supply to the initialHolder', async function () {
+    expect(await this.token.balanceOf(initialHolder)).to.be.bignumber.equal(TOTAL_SUPPLY);
+  });
+
+  describe('_transfer', function () {
+    const amount = new BN(5000000);
+
+    beforeEach(async function () {
+      this.receipt = await this.token.transfer(recipient, amount);
+    });
+
+    describe('when transfer executed normally', function () {
+      it('recipient\'s balances increases by the amount', async function () {
+        expect(await this.token.balanceOf(recipient)).to.be.bignumber.equal(amount);
+      });
+      it('emits a Transfer event', async function () {
+        expectEvent(this.receipt, 'Transfer', {
+          from: initialHolder,
+          to: recipient,
+          value: amount,
+        });
+      });
+    });
+    describe('when the sender is the zero address', function () {
+      it('reverts', async function () {
+        await expectRevert(this.token.transferFrom(ZERO_ADDRESS, recipient, amount),
+          'ERC20: transfer from the zero address',
+        );
+      });
+    });
+    describe('when the recipient is the zero address', function () {
+      it('reverts', async function () {
+        await expectRevert(this.token.transfer(ZERO_ADDRESS, amount),
+          'ERC20: transfer to the zero address',
+        );
+      });
+    });
   });
 });
